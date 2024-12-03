f07c4970bb061471935792ae02ee09668fe42180
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2024-12-03 09:56:49 +0100

    no capacitor

--- PiggyPenny/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.piggypenny',
-  appName: 'piggypenny',
-  webDir: 'build'
-};
-
-export default config;
